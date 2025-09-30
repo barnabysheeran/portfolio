4634cf1f394439f8e84c11ba71018f2e55e67c7a
Barnaby Sheeran <barnabysheeran@gmail.com>
2025-09-30 18:07:26 +0100

    feat: create SiteFooter component and apply styles from SiteFooter.module.css

--- src/ts/ui/component/site/footer/SiteFooter.tsx
+++ src/ts/ui/component/site/footer/SiteFooter.tsx
@@ -1,3 +1,5 @@
+import styles from './SiteFooter.module.css';
+
 export default function SiteFooter() {
-  return <div>Site Footer</div>;
+  return <div className={styles['site-footer']}>Site Footer</div>;
 }
