9cb7b8f5aa9f79a6c1da2d31bd7abe7add00ec24
Barnaby Sheeran <barnabysheeran@gmail.com>
2025-09-30 16:34:10 +0100

    feat: implement initial styles for Application component

--- src/ts/Application.tsx
+++ src/ts/Application.tsx
@@ -1,16 +1,11 @@
-import './Application.module.css'
+import styles from './Application.module.css'
 
 import { ButtonDevelopment } from './component/button/ButtonDevelopment';
 
-function App() {
-
-  // _______________________________________________________________ Development
-
+export default function Application() {
   return (
-    <>
+    <div className={styles['application']}>
       <ButtonDevelopment label="Development Button" onClick={() => alert('Button Clicked!')} />
-    </>
+    </div>
   )
 }
-
-export default App
