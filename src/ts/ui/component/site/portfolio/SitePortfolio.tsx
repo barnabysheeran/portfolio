1d8ad9f8cacce13229f46af30411cd61d88f61d7
Barnaby Sheeran <barnabysheeran@gmail.com>
2025-09-30 17:34:54 +0100

    feat: create SitePortfolio.module.css and apply styles in SitePortfolio component

--- src/ts/ui/component/site/portfolio/SitePortfolio.tsx
+++ src/ts/ui/component/site/portfolio/SitePortfolio.tsx
@@ -1,3 +1,5 @@
+import styles from './SitePortfolio.module.css';
+
 export default function SitePortfolio() {
-  return <div>Site Portfolio</div>;
+  return <div className={styles['site-portfolio']}>Site Portfolio</div>;
 }
