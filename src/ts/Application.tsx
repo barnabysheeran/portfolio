182da56678008aa80c7613202dcc99ae4e726b8d
Barnaby Sheeran <barnabysheeran@gmail.com>
2025-09-30 16:47:56 +0100

    feat: update Application component to include UI and adjust body styles in index.css

--- src/ts/Application.tsx
+++ src/ts/Application.tsx
@@ -1,11 +1,14 @@
 import styles from './Application.module.css'
 
+import { UI } from './ui/UI';
+
 import { ButtonDevelopment } from './ui/component/button/ButtonDevelopment';
 
 export default function Application() {
   return (
     <div className={styles['application']}>
       <ButtonDevelopment label="Development Button" onClick={() => alert('Button Clicked!')} />
+      <UI />
     </div>
   )
 }
