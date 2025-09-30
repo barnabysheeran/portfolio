95915ef4b673403fe46b9a2aa5c94939d59801f0
Barnaby Sheeran <barnabysheeran@gmail.com>
2025-09-30 17:00:17 +0100

    feat: add SitePortfolio and SiteEVE components to UI and remove ButtonDevelopment from Application

--- src/ts/ui/UI.tsx
+++ src/ts/ui/UI.tsx
@@ -1,12 +1,14 @@
 import SiteHeader from "./component/site/header/SiteHeader";
-
+import SitePortfolio from "./component/site/portfolio/SitePortfolio";
+import SiteEVE from "./component/site/eve/SiteEVE";
 import SiteFooter from "./component/site/footer/SiteFooter";
 
 export default function UI() {
   return (
     <div>
       <SiteHeader />
-
+      <SitePortfolio />
+      <SiteEVE />
       <SiteFooter />
     </div>
   );
---
+++ src/ts/ui/component/site/portfolio/SitePortfolio.tsx
@@ -0,0 +1,3 @@
+export default function SitePortfolio() {
+  return <div>Site Portfolio</div>;
+}
