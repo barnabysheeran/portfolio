ed54418a3eab0e860984d78027fb4fff111475af
Barnaby Sheeran <barnabysheeran@gmail.com>
2025-09-30 17:48:08 +0100

    feat: update SiteHeader to use framer-motion for animation and adjust SitePortfolio styles

--- src/ts/ui/component/site/header/SiteHeader.tsx
+++ src/ts/ui/component/site/header/SiteHeader.tsx
@@ -1,5 +1,15 @@
 import styles from './SiteHeader.module.css';
+import { motion } from 'framer-motion';
 
 export default function SiteHeader() {
-  return <div className={styles['site-header']}>Site Header</div>;
+  return (
+    <motion.div
+      className={styles['site-header']}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1.0, delay: 1.0 }}
+    >
+      /////
+    </motion.div>
+  );
 }
