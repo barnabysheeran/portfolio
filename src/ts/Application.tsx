4daef97d6b3c7c9ee454047bea183985c5a1ecf2
Barnaby Sheeran <barnabysheeran@gmail.com>
2025-10-01 06:21:09 +0100

    feat: implement Engine class for managing application lifecycle and rendering

--- src/ts/Application.tsx
+++ src/ts/Application.tsx
@@ -1,8 +1,27 @@
+import { useEffect } from 'react';
+
 import styles from './Application.module.css'
+
+import Engine from './engine/Engine';
 
 import UI from './ui/UI';
 
 export default function Application() {
+
+  // ____________________________________________________________________ Engine
+
+  // Initialise Engine Once
+  useEffect(() => {
+    Engine.init();
+    Engine.start();
+
+    return () => {
+      Engine.stop();
+    }
+  }, []);
+
+  // ____________________________________________________________________ Layout
+
   return (
     <div className={styles['application']}>
       <UI />
---
+++ src/ts/engine/Engine.ts
@@ -0,0 +1,48 @@
+export default class Engine {
+
+    static #applicationRunTimeMS = 0;
+
+    static #isInitialized = false;
+    static #isRunning = false;
+
+    // _________________________________________________________________________
+
+    static init() {
+        // Initialize Once Only
+        if (this.#isInitialized) return;
+
+        // Start Main Loop
+        requestAnimationFrame(this.#tick.bind(this));
+
+        // Initialized
+        this.#isInitialized = true;
+    }
+
+    // ______________________________________________________________ Start Stop
+
+    static start() {
+        this.#isRunning = true;
+    }
+
+    static stop() {
+        this.#isRunning = false;
+    }
+
+    // ____________________________________________________________________ Tick
+
+    static #tick(applicationRunTimeMS: number) {
+        // Loop
+        requestAnimationFrame(this.#tick.bind(this));
+
+        // Running ?
+        if (!this.#isRunning) return;
+
+        // Delta Time
+        const deltaTimeMS = applicationRunTimeMS - this.#applicationRunTimeMS;
+
+        console.log(`Engine Tick - Δ: ${deltaTimeMS}ms - T: ${applicationRunTimeMS}ms`);
+        
+        // Store
+        this.#applicationRunTimeMS = applicationRunTimeMS;
+    }
+}
