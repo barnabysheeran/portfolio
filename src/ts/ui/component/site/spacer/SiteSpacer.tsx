04743085d7957fc0a20f4d05dda675e4ee74f9e4
Barnaby Sheeran <barnabysheeran@gmail.com>
2025-10-01 07:06:09 +0100

    fix: correct opacity values in animation for SiteSpacer component

--- src/ts/ui/component/site/spacer/SiteSpacer.tsx
+++ src/ts/ui/component/site/spacer/SiteSpacer.tsx
@@ -7,7 +7,7 @@ export default function SiteSpacer() {
       className={styles['site-spacer']}
       initial={{ opacity: 0 }}
       animate={{
-        opacity: [0, 1, 1, 0, 0], 
+        opacity: [0, 1, 1, 1, 0], 
         transition: {
           duration: 4.0,
           times: [0.0, 0.2, 0.4, 0.6, 1.0],
