e29369e35c751af295de3b8cffd165f50ee13ed3
Barnaby Sheeran <barnabysheeran@gmail.com>
2025-10-03 16:54:35 +0100

    feat: integrate ButtonColorCircle into SiteOverlay and refactor MenuIconFlyout for theme selection

--- src/ts/ui/component/menu/flyout/MenuIconFlyout.tsx
+++ src/ts/ui/component/menu/flyout/MenuIconFlyout.tsx
@@ -1,61 +1,27 @@
-import { useState } from 'react';
-import useUiState from '../../../../store/uiState';
-
-import { ButtonIcon } from '../../button/icon/ButtonIcon';
+import { useState, type ReactNode } from 'react';
 
 import styles from './MenuIconFlyout.module.css';
 
-export default function MenuIconFlyout() {
+interface MenuIconFlyoutProps {
+  trigger: (toggle: () => void) => ReactNode;
+  children: ReactNode;
+}
 
-  // Local State not Stored in uiStore
+export default function MenuIconFlyout({ trigger, children }: MenuIconFlyoutProps) {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  // ____________________________________________________________ Click Handlers
-
   const handleToggleOpenClose = () => {
-    // Toggle
     setIsMenuOpen(!isMenuOpen);
   };
 
-  const handleClickRed = () => {
-    useUiState.getState().setTheme('red');
-
-    // Close Menu
-    setIsMenuOpen(false);
-  }
-
-  const handleClickBlue = () => {
-    useUiState.getState().setTheme('blue');
-
-    // Close Menu
-    setIsMenuOpen(false);
-  }
-
-  const handleClickGreen = () => {
-    useUiState.getState().setTheme('green');
-
-    // Close Menu
-    setIsMenuOpen(false);
-  };
-
-  const handleClickBlack = () => {
-    useUiState.getState().setTheme('black');
-
-    // Close Menu
-    setIsMenuOpen(false);
-  }
-
-  // ____________________________________________________________________ Render
-
-  return <div className={styles['menu-icon-flyout']}>
-    <ButtonIcon size="large" icon="☰" onClick={handleToggleOpenClose} />
-    {isMenuOpen && (
-      <div className={styles['flyout-menu']}>
-        <ButtonIcon size="large" icon="🔴" onClick={handleClickRed} />
-        <ButtonIcon size="large" icon="🔵" onClick={handleClickBlue} />
-        <ButtonIcon size="large" icon="🟢" onClick={handleClickGreen} />
-        <ButtonIcon size="large" icon="⚫" onClick={handleClickBlack} />
-      </div>
-    )}
-  </div>;
+  return (
+    <div className={styles['menu-icon-flyout']}>
+      {trigger(handleToggleOpenClose)}
+      {isMenuOpen && (
+        <div className={styles['flyout-menu']}>
+          {children}
+        </div>
+      )}
+    </div>
+  );
 }
--- src/ts/ui/component/site/overlay/SiteOverlay.tsx
+++ src/ts/ui/component/site/overlay/SiteOverlay.tsx
@@ -1,9 +1,40 @@
+import useUiState from '../../../../store/uiState';
+
 import MenuIconFlyout from '../../menu/flyout/MenuIconFlyout';
+import { ButtonColorCircle } from '../../button/colorCircle/ButtonColorCircle';
+
 
 import styles from './SiteOverlay.module.css';
 
 export default function SiteOverlay() {
-    return <div className={styles['site-overlay']}>
-        <MenuIconFlyout />
-    </div>;
+    const handleClickRed = () => {
+        useUiState.getState().setTheme('red');
+    };
+
+    const handleClickBlue = () => {
+        useUiState.getState().setTheme('blue');
+    };
+
+    const handleClickGreen = () => {
+        useUiState.getState().setTheme('green');
+    };
+
+    const handleClickBlack = () => {
+        useUiState.getState().setTheme('black');
+    };
+
+    return (
+        <div className={styles['site-overlay']}>
+            <MenuIconFlyout
+                trigger={(toggle) => (
+                    <ButtonColorCircle size="medium" color="red" onClick={toggle} />
+                )}
+            >
+                <ButtonColorCircle size="medium" color="red" onClick={handleClickRed} />
+                <ButtonColorCircle size="medium" color="blue" onClick={handleClickBlue} />
+                <ButtonColorCircle size="medium" color="green" onClick={handleClickGreen} />
+                <ButtonColorCircle size="medium" color="black" onClick={handleClickBlack} />
+            </MenuIconFlyout>
+        </div>
+    );
 }
