92b2f04d87607f3ace6d5f6c06dce6ecf1a538c7
Barnaby Sheeran <barnabysheeran@gmail.com>
2025-10-03 16:32:46 +0100

    feat: implement theme selection in MenuIconFlyout and remove theme buttons from SiteHeader

--- src/ts/ui/component/menu/flyout/MenuIconFlyout.tsx
+++ src/ts/ui/component/menu/flyout/MenuIconFlyout.tsx
@@ -1,13 +1,49 @@
+import { useState } from 'react';
+import useUiState from '../../../../store/uiState';
+
 import { ButtonIcon } from '../../button/icon/ButtonIcon';
 
 import styles from './MenuIconFlyout.module.css';
 
 export default function MenuIconFlyout() {
 
+  // Local State not Stored in uiStore
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  // ____________________________________________________________ Click Handlers
+
+  const handleToggleOpenClose = () => {
+    // Toggle
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const handleClickRed = () => {
+    useUiState.getState().setTheme('red');
+  }
+
+  const handleClickBlue = () => {
+    useUiState.getState().setTheme('blue');
+  }
+
+  const handleClickGreen = () => {
+    useUiState.getState().setTheme('green');
+  };
+
+  const handleClickBlack = () => {
+    useUiState.getState().setTheme('black');
+  }
 
   // ____________________________________________________________________ Render
 
   return <div className={styles['menu-icon-flyout']}>
-    <ButtonIcon size="large" icon="☰" onClick={() => alert('Menu clicked!')} />
+    <ButtonIcon size="large" icon="☰" onClick={handleToggleOpenClose} />
+    {isMenuOpen && (
+      <div className={styles['flyout-menu']}>
+        <ButtonIcon size="large" icon="🔴" onClick={handleClickRed} />
+        <ButtonIcon size="large" icon="🔵" onClick={handleClickBlue} />
+        <ButtonIcon size="large" icon="🟢" onClick={handleClickGreen} />
+        <ButtonIcon size="large" icon="⚫" onClick={handleClickBlack} />
+      </div>
+    )}
   </div>;
 }
