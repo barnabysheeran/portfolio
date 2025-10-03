f3fc7f5f4667f212eea27235c370e31dba0146e8
Barnaby Sheeran <barnabysheeran@gmail.com>
2025-10-03 12:42:58 +0100

    feat: update color variables in colours.css, adjust theme properties in UI components, and enhance SiteHeader with theme selection buttons

--- src/ts/ui/component/site/header/SiteHeader.tsx
+++ src/ts/ui/component/site/header/SiteHeader.tsx
@@ -1,7 +1,35 @@
+import useUiState from '../../../../store/uiState';
+
+import { ButtonIcon } from '../../button/icon/ButtonIcon';
+
 import styles from './SiteHeader.module.css';
 
 export default function SiteHeader() {
+
+  const handleClickRed = () => {
+    useUiState.getState().setTheme('red');
+  }
+
+  const handleClickBlue = () => {
+    useUiState.getState().setTheme('blue');
+  }
+
+  const handleClickGreen = () => {
+    useUiState.getState().setTheme('green');
+  }
+
+  const handleClickBlack = () => {
+    useUiState.getState().setTheme('black');
+  }
+
+  // ____________________________________________________________________ Render
+
   return (
-    <div className={styles['site-header']}></div>
+    <div className={styles['site-header']}>
+      <ButtonIcon size="small" onClick={handleClickRed} icon="🔴" />
+      <ButtonIcon size="small" onClick={handleClickBlue} icon="🔵" />
+      <ButtonIcon size="small" onClick={handleClickGreen} icon="🟢" />
+      <ButtonIcon size="small" onClick={handleClickBlack} icon="⚫" />
+    </div>
   );
 }
