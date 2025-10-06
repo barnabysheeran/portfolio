4042eaae66beb9874e5f10d6c9eac39a2e747f3f
Barnaby Sheeran <barnabysheeran@gmail.com>
2025-10-06 17:11:02 +0100

    refactor: Replace GlassMediaHolder with ImageSlider and update background color in SiteGlass

--- src/ts/ui/site/glass/SiteGlass.tsx
+++ src/ts/ui/site/glass/SiteGlass.tsx
@@ -1,16 +1,14 @@
-import GlassMediaHolder from './glass-media/GlassMediaHolder';
+import ImageSlider from '../../component/image-slider/ImageSlider';
 import GlassHolder from './glass/GlassHolder';
 
 import styles from './SiteGlass.module.css';
 
 export default function SiteGlass() {
-  // _______________________________________________________________ Glass Media
-
   // ____________________________________________________________________ Render
 
   return (
     <div className={styles['site-glass']}>
-      <GlassMediaHolder />
+      <ImageSlider />
       <GlassHolder />
     </div>
   );
