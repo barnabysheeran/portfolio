79506c27842d185ab91a0dcbc056739264481a31
Barnaby Sheeran <barnabysheeran@gmail.com>
2025-10-07 16:18:45 +0100

    feat: Add animation support with duration variable and update button/icon styles

--- src/ts/ui/site/glass/glass/directable/dotmatrix/view/header/ViewHeader.ts
+++ src/ts/ui/site/glass/glass/directable/dotmatrix/view/header/ViewHeader.ts
@@ -73,7 +73,7 @@ export default class DotMatrixViewHeader extends DotMatrixView {
     // Create Glyph Line Centered Component
     const COMPONENT = new ComponentGlyphLineCentered(
       this.SHAPE_MANAGER,
-      'PROJECT',
+      'PROJECTS',
       this.#gridY,
       delayFrames +
         DirectableDotMatrixConstants.getDelayFromGridPosition(0, this.#gridY),
