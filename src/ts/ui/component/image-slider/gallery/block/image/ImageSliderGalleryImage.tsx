09b8ec2419c0b7ad3c2358e80db9517b04ce188f
Barnaby Sheeran <barnabysheeran@gmail.com>
2025-10-07 10:37:38 +0100

    feat: Add ImageSliderGallery and ImageSliderGalleryImage components with corresponding styles

---
+++ src/ts/ui/component/image-slider/gallery/block/image/ImageSliderGalleryImage.tsx
@@ -0,0 +1,5 @@
+import styles from './ImageSliderGalleryImage.module.scss';
+
+export const ImageSliderGalleryImage = () => {
+  return <div className={styles['image-slider-gallery-image']}></div>;
+};
