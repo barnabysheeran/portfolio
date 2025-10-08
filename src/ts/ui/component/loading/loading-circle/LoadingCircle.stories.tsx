082891a4569b17fb75f24654a6d43fd9d2ade4d0
Barnaby Sheeran <barnabysheeran@gmail.com>
2025-10-08 14:54:59 +0100

    feat: add LoadingCircle component with styles and stories

---
+++ src/ts/ui/component/loading/loading-circle/LoadingCircle.stories.tsx
@@ -0,0 +1,8 @@
+import LoadingCircle from './LoadingCircle';
+
+export default {
+  title: 'Loading/LoadingCircle',
+  component: LoadingCircle,
+};
+
+export const Default = () => <LoadingCircle />;
---
+++ src/ts/ui/component/loading/loading-circle/LoadingCircle.tsx
@@ -0,0 +1,5 @@
+import styles from './LoadingCircle.module.css';
+
+export default function LoadingCircle() {
+  return <div className={styles['loading-circle']}>Loading Circle</div>;
+}
